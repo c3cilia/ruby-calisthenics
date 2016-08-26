3515ba0631893d37491a762d23615cbb0222af42
c3cilia <c3sscoder@gmail.com>
2016-08-26 15:32:53 +0300

    finished the attr_accessor with history exercise

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -4,7 +4,16 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}=(value)
+      	if !defined? @#{attr_name}_history
+      		@#{attr_name}_history = [@#{attr_name}]
+      	end 
+      	@#{attr_name} = value 
+      	if @#{attr_name}_history.size < 2
+      		@#{attr_name}_history << @#{attr_name}
+      	end 
+      end
+
     }
   end
 end
